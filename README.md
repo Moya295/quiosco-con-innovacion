507337c18a227a36463be9517880b6d6b98c1934
Eduardo Moya <21030769@itcelaya.edu.mx>
2024-06-04 18:13:47 -0600

    primer commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# Quiosco de Comida
+
+Este proyecto es parte del curso de Udemy "Aprende React desde cero: Hooks y State hasta TypeScript, Zod, Zustand, React Query, Next.js, React Router, MERN y PERN" impartido por Juan Pablo De la Torre Valdez. El objetivo del proyecto es desarrollar una aplicación de quiosco de comida que incluye una interfaz de usuario y una interfaz de administrador, similar a Uber Eats para un restaurante.
+
+## Características (27-marzo-2024)
+
+- **Interfaz de Usuario (Frontend)**
+  - Visualización del menú de productos.
+  - Selección de productos para el carrito de compras.
+  - Gestión de pedidos en tiempo real.
+  - Autenticación de usuarios.
+  - Resumen de pedidos y proceso de pago.
+  
+- **Interfaz de Administrador (Backend)**
+  - Gestión de productos (crear, actualizar, eliminar).
+  - Gestión de categorías de productos.
+  - Visualización y gestión de pedidos en tiempo real.
+  - Informes de ventas y estadísticas.
+  
+
+## Características (29-marzo-2024)  
+
+- **Interfaz de visualizacion de órdenes (Frontend)**
+  - Gestión de productos (crear, actualizar, eliminar).
+  - Gestión de categorías de productos.
+  - Visualización y gestión de pedidos en tiempo real.
+  - Informes de ventas y estadísticas.
+
+
+- **Lo unico que queda por hacer es implementar algo innovador al proyecto   
+
+
+## Características (04-junio-2024)
+
+  -Integración de sistema de recomendaciones personalizadas.
+  -Implementa un sistema de recomendaciones que sugiera platillos basados en las preferencias y el historial de pedidos del usuario. 
+ 
+  
+ Eduardo Moya Zamarripa v3 (Final)
+
---
+++ src/schema/index.ts
@@ -0,0 +1,63 @@
+import { CiPassport1 } from 'react-icons/ci'
+import { z } from 'zod'
+
+export const OrderSchema = z.object({
+    name: z.string().min(1, 'Tu Nombre es Obligatorio'),
+    total: z.number().min(1, 'Hay errores en el pedido'),
+    order: z.array(z.object({
+        id: z.number(),
+        name: z.string(),
+        price: z.number(),
+        quantity: z.number(),
+        subtotal: z.number()
+    }))
+})
+
+export const OrderIdSchema = z.object({
+    orderId: z.string().transform((value) => parseInt(value))
+                       .refine(value => value>0, {message:'Hay errores'})
+})
+
+export const SearchSchema = z.object({
+    search: z.string()
+                .trim()
+                .min(1, {message: 'La búsqueda no puede ir vacía'})
+})
+
+export const ProductSchema = z.object({
+    name: z.string()
+        .trim()
+        .min(1, { message: 'El Nombre del Producto no puede ir vacio'}),
+    price: z.string()
+        .trim()
+        .transform((value) => parseFloat(value)) 
+        .refine((value) => value > 0, { message: 'Precio no válido' })
+        .or(z.number().min(1, {message: 'La Categoría es Obligatoria' })),
+    categoryId: z.string()
+        .trim()
+        .transform((value) => parseInt(value)) 
+        .refine((value) => value > 0, { message: 'La Categoría es Obligatoria' })
+        .or(z.number().min(1, {message: 'La Categoría es Obligatoria' })),
+    image: z.string().min(1, {message: 'La Imagen es Obligatoria'})
+})
+
+export const LoginSchema = z.object({
+    email: z.string()
+        .trim()
+        .min(1, {message: 'Debe ingresar su Email'}),
+    pass: z.string()
+        .trim()
+        .min(1, {message: 'Debe ingresar su Contraseña'})
+})
+
+export const UserSchema = z.object({
+    name: z.string()
+        .trim()
+        .min(1, {message: 'Debe ingresar su Nombre'}),
+    email: z.string()
+        .trim()
+        .min(1, {message: 'Debe ingresar su Email'}),
+    pass: z.string()
+        .trim()
+        .min(1, {message: 'Debe ingresar su Contraseña'})
+})
